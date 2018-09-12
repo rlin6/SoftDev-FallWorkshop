f66d67cc8d736cbf6d57486eded4cacea7c47282
rlin6 <rlin6@stuy.edu>
2018-09-12 18:42:20 -0400

    added 02_randomizer

---
+++ 02_randomizer/LINgeringQuestions_linK-linR.py
@@ -0,0 +1,32 @@
+#LINgeringQuestions -- Kevin Lin, Ricky Lin
+#SoftDev1 pd06
+#K02 NO-body expects the Spanish Inquisition!
+#2018-09-11
+
+#uses random library for randomization
+import random
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', "Theodore (Don't really care)", 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+
+}
+
+#puts all the teams into one array to pick from
+teams = ['w', 'm', 'x']
+
+def randomizer():
+    #choose a team or go random
+    pick = input("Pick w, m, x or type something else for random \n")
+    if pick in KREWES.keys():
+        print (random.choice(KREWES[pick]))
+    else:
+        print(random.choice(KREWES[random.choice(teams)]));
+
+randomizer()
+
+
