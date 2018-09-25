27b9bc429cd841eececca4a429848762cba8c3bd
rlin6 <rlin6@stuy.edu>
2018-09-25 08:36:53 -0400

    add util

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -3,9 +3,9 @@
 #K10: Jinja Tuning ...
 #2018-09-24
 
-import random
-from flask import Flask, render_template
-from RHCP import occCreate, randomOcc, OCCLIST
+#import flask methods
+from flask import Flask, render_template 
+from util import RHCP #import util as package
 
 app = Flask(__name__)
 
@@ -15,9 +15,16 @@ def hello_World():
 
 @app.route("/occupations")
 def occupations():
-    occDict = occCreate("data/occupations.csv", OCCLIST, 0, 2)
-    occChosen = randomOcc(occDict) 
-    return render_template('LivingTribunal_chenD-linR.html',random = occChosen, link = occDict[occChosen][1], _dict = occDict)
+    #create a new empty dict
+    occDict = {}
+    #create the dictionary reading 3 columns of the csv 
+    occDict = RHCP.occCreate("data/occupations.csv", occDict, 0, 2) 
+    #pick a random occupation
+    occChosen = RHCP.randomOcc(occDict) 
+    return render_template('LivingTribunal_chenD-linR.html',
+                            random = occChosen, #random job that appears
+                            link = occDict[occChosen][1], #link of the random
+                            _dict = occDict) #use dictionary in Jinja
 
 if __name__ == '__main__':
     app.run(debug=True)
