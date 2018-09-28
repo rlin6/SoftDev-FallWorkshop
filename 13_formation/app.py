6977c24cc884c3bdd26a1ab9879bc0f34f74ae67
unknown <rlin6@stuy.edu>
2018-09-28 18:29:52 -0400

    fixed

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -12,13 +12,15 @@ def home():
     print(app)
     return render_template("index.html")
 
-@app.route("/auth", methods=["GET", "POST"])
+@app.route("/auth", methods=["POST"])
 def auth():
     print(app)
     print(request)
     print(request.args)
+    print(request.form['first'])
+    print(request.form['last'])
     user = request.form['first'] + ' ' + request.form['last'] 
-    return "Welcome " + user + " to my den. <br> You have reached here through a POST method" + '''<br><a href="/">If you want to leave</a>'''
+    return "Welcome " + user + " to my den. <br> You have reached here through a " + request.method + ' method' + '''<br><a href="/">If you want to leave</a>'''
 
 if __name__ == '__main__':
     app.run(debug=True)
