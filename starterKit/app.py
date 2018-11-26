b44d1194b904d1ed76d13a78395372ad33c2c8af
rlin6 <rlin6@stuy.edu>
2018-11-26 12:06:16 -0500

    update starter kit

--- starterKit/app.py
+++ starterKit/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print(app)
-    return render_template("input.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
